8a4b6e47d45a1c6fc636baf2ec0dd1105702bb02
MarkMwago2 <markmwago@gmail.com>
2019-04-08 11:34:36 +0300

    pass first test

---
+++ src/test/java/CeaserCipherrTest.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CeaserCipherrTest {
+
+    @Test
+    public void CeaserCipherrTest_listAlphabet_ArrayList() {
+        CeaserCipherrTest testPingPong = new CeaserCipherrTest();
+        List<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.char();
+        assertEquals(expectedOutput, testCeaserCipherrTest.runCeaserCipherr());
+
+
+    }
+}
+
