eaf84974bc8a6bc70c93ec0b6f2fcd754bc5b102
MarkMwago2 <markmwago@gmail.com>
2019-04-24 11:55:53 +0300

    add getters and setters in encoding and decoding class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,2 @@
-import java.io.Console;
-
 public class App {
-    public static void main(String[] args) {
-        Console myConsole= System.console();
-        System.out.println("Enter your message");
-        CeaserCipherr myCeasarCipherr = new CeaserCipherr();
-        String myInputtedString = myConsole.readLine();
-
-        String encryptedResult =  myCeasarCipherr.runCeaserCipherr(myInputtedString, 3);
-        System.out.println(encryptedResult);
-    }
 }
